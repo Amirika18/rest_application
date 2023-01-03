4f9fd07e29d28a71cdadae3e4a5187d686b558a5
enaumova <enaumova@cinimex.ru>
2023-01-03 13:12:12 +0300

    API path for interface

--- params.js
+++ params.js
@@ -1,4 +1,4 @@
 // const url_db = "http://192.168.50.191:3000"
 // const url_db = "http://192.168.6.41:3000"
-const url_db = "http://localhost"
+const url_db = "http://85.236.189.44:8112/"
 export default url_db
